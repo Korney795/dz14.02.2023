b2fd3ccaefdda9e6dcfb0c83d0d40bc650824dc2
Korney795 <Dmitriy795@mail.ru>
2023-02-15 19:40:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+#
+#li=[]
+#size=int(input())
+#for i in range(size):
+#    li.append(int(input()))
+#for i in li:
+ #   if i%2==0:
+ #       print(i)
+
+
+
+
+#li=[]
+#size=int(input())
+#for i in range(size):
+#    li.append(int(input()))
+#count=0
+#for i in li:
+  #  if i>0:
+  #      count+=1
+#print(count)
+
+
+
+#li=[]
+#size=int(input())
+#for i in range(size):
+ #   li.append(int(input()))
+#for i in range(1,li[i]):
+  #  if li[i]>li[i-1]:
+  #    print(li[i],end="")
+
+
+
+
+#li=[]
+#size=int(input())
+#for i in range(size):
+ #li.append(int(input()))
+
+#for i in range(1,li[i]):
+   # if li[i]*li[i-1]>0 or li[i]==0 and li[i-1]==0:
+   #    print (li[i],li[i-1])
+   #     break
+
+
+
+# 1.df.5.56.6.df.g4
+
+s=input()
+count=0
+for i in s:
+    if i.isdigit():
+        count+=1
+print(count)
